78ca304c20508f56d866eca5fe1b0590f0f7a822
M2208 <mauriced@live.nl>
2015-11-04 14:46:00 +0100

    Delete scripts.js

--- Web/Opdrachten/scripts.js
+++
@@ -1,8 +0,0 @@
- "use strict";
-var angle = 0;
-function klikken(){
-    console.log("click registered");
-    var spin = document.getElementById("pg");
-    angle+=90;
-    spin.style.transform = "rotate("+angle+"deg)";
-}
